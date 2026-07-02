4306a7d4d06dce22fc16c49b76d70ec4a2e1bafe
Jeevadharshini V <jeevidharshu25@gmail.com>
2026-07-02 13:12:28 +0530

    Add problem test case and leaderboard entities

---
+++ src/main/java/com/codearena/codearena/model/TestCase.java
@@ -0,0 +1,75 @@
+package com.codearena.codearena.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "test_cases")
+public class TestCase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long problemId;
+
+    @Column(columnDefinition = "TEXT")
+    private String input;
+
+    @Column(columnDefinition = "TEXT")
+    private String expectedOutput;
+
+    private Boolean hidden;
+
+    private Integer timeLimitOverride;
+
+    public TestCase() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getProblemId() {
+        return problemId;
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public String getExpectedOutput() {
+        return expectedOutput;
+    }
+
+    public Boolean getHidden() {
+        return hidden;
+    }
+
+    public Integer getTimeLimitOverride() {
+        return timeLimitOverride;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setProblemId(Long problemId) {
+        this.problemId = problemId;
+    }
+
+    public void setInput(String input) {
+        this.input = input;
+    }
+
+    public void setExpectedOutput(String expectedOutput) {
+        this.expectedOutput = expectedOutput;
+    }
+
+    public void setHidden(Boolean hidden) {
+        this.hidden = hidden;
+    }
+
+    public void setTimeLimitOverride(Integer timeLimitOverride) {
+        this.timeLimitOverride = timeLimitOverride;
+    }
+}
